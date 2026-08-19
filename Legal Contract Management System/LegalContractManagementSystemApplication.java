b7ca37b3b077646a8bf7a67a903459bf98a84cb9
Prajjwal Naykawadi <naykawadiprajjwal@gmail.com>
2026-08-19 20:09:06 +0530

    Add files via upload

---
+++ Legal Contract Management System/LegalContractManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.legalcontract.legal_contract_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegalContractManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegalContractManagementSystemApplication.class, args);
+	}
+
+}
